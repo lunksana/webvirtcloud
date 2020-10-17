c8bb5157c38b4a84b437ccc8a4664701897512fe
lunksana <lunksana@users.noreply.github.com>
2020-10-17 17:32:31 +0800

    Update and rename settings.py.template to settings.py
    settings.py rename

--- webvirtcloud/settings.py
+++ webvirtcloud/settings.py
@@ -8,7 +8,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = ''
+SECRET_KEY = '}^sSc10A3[37w&k!ALMr=[OZg9wo&JQQ=pf|[;M,cmuryLMsK}'
 
 DEBUG = False
 
